5217c3262b741adafe2098b899d6f276bf2cb1e5
zlatkos94 <34042716+zlatkos94@users.noreply.github.com>
2018-02-01 19:31:03 +0100

    ispravljena verzija (aktivnost)

--- Redomat/referada/JSONgetResult.php
+++ Redomat/referada/JSONgetResult.php
@@ -7,11 +7,16 @@ $db=mysql_select_db("redomat",$connection);
 $querry= mysql_query("select broj_korisnik as id, broj, sifra_aktivnost as aktivnost, oib from obrada where status_obrade =0 order by broj_korisnik asc limit 1;", $connection);
 
 $row = mysql_fetch_assoc($querry);
+$id=$row['aktivnost'];
+
+$querry= mysql_query("select naziv_aktivnost from aktivnost where sifra_aktivnost=$id", $connection);
+$row2=mysql_fetch_assoc($querry);
+$ime=$row2["naziv_aktivnost"];
 	if($row['id']!=''){
 		$rows['id']= $row['id'];
 		$rows['broj']= $row['broj'];
 		$rows['oib']= $row['oib'];
-		$rows['aktivnost']= $row['aktivnost'];
+		$rows['aktivnost']= $ime;
 	}
 	else{
 		$rows['id']='--';
